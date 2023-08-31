b03cc1074569b01fc603fd9cff58d129165948d9
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-08-31 15:26:34 +0300

    new commit

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,21 @@
 class MySet:
+    def __init__(self, enumerable=None):
+        if enumerable is None:
+            enumerable = []
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
 
-    pass
+    def has(self,value):
+        return (value in self.dictionary)
+    
+    def add(self,value):
+        self.dictionary[value]= True
+        return self
+    
+    def delete(self,value):
+        self.dictionary.pop(value,None)
+        return self
+    
+    def size(self):
+        len(self.dictionary)
